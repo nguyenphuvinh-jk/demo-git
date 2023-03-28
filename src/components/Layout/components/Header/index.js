1efcb1537720e4d1a9f66a26d2eb7e6caf6f5e4f
nguyenphuvinh-jk <nguyenphuvinh676@gmail.com>
2023-03-28 20:55:05 +0700

    Xay dung layout mac dinh

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,14 @@
+import styles from './Header.module.scss'
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles)
 function Header() {
     return ( 
-        <h1>Header</h1>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+
+            </div>
+        </header>
      );
 }
 
